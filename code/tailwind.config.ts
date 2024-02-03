0c8d1cd57abc4c27b35fd22255c036653e1e906c
Rachit Bharadwaj <mail.rachit.bharadwaj@gmail.com>
2024-02-03 12:23:25 +0530

    added colors and screen sizes in tailwind config

--- code/tailwind.config.ts
+++ code/tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./components/**/*.{ts,tsx}",
+    "./containers/**/*.{ts,tsx}",
+    "./templates/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -27,10 +27,36 @@ const config = {
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
+          100: "#eeeeee",
+          200: "#d7d4d5",
+          300: "#a8a8a8",
+          400: "#414b55",
+          500: "#2b323d",
+          600: "#1d2125",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
+          100: "#76b3f0",
+          200: "#0078b6",
+          300: "#13416a",
+        },
+        red: {
+          100: "#ffc7c5",
+          200: "#ff5455",
+          300: "#ff191a",
+          400: "#520707",
+        },
+        green: {
+          100: "#b7f39b",
+          200: "#42c306",
+          300: "#29a61e",
+          400: "#12620c",
+        },
+        yellow: {
+          100: "#fedfb1",
+          200: "#f8c67e",
+          300: "#e49c34",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
@@ -72,9 +98,24 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+
+      // media queries
+      screens: {
+        xs: "20rem", // 320px
+
+        sm: "40rem", // 640px
+
+        md: "48rem", // 768px
+
+        lg: "64rem", // 1024px
+
+        xl: "80rem", // 1280px
+
+        "2xl": "96rem", // 1536px
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
